e44e978b6c2c91a35cb1e1113a24fa502654008f
Ruchi Badkur <94377483+RuchiBadkur@users.noreply.github.com>
2024-03-19 22:02:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-let team = players;
-let team1 = [...players];
+const team = players;
+const team1 = [...players];
 
-let cap1 = Object.assign({}, person);
+const cap1 = Object.assign({}, person);
+// const cap1 = {...person};
 
 // console.log(team, team1, cap1);
 
